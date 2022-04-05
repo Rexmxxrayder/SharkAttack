2bed3e6fa1429a312ac665a8a54ca9941699a654
Wizerdy <grancru2000@gmail.com>
2022-04-05 15:27:30 +0200

    DragCamera

---
+++ SharkAttack/Assets/Scripts/SwitchCameraOnDrag.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCameraOnDrag : MonoBehaviour {
+    [SerializeField] InputController _input;
+    [SerializeField] ChooseCamera _cameraMovements;
+    [SerializeField] float _minDistance = 2f;
+
+    private void Reset() {
+        _input = GetComponent<InputController>();
+        _cameraMovements = GetComponent<ChooseCamera>();
+    }
+
+    void Awake() {
+        if (_input != null) {
+            _input.OnDrag += CameraMove;
+        }
+    }
+
+    void OnDestroy() {
+        if (_input != null) {
+            _input.OnDrag += CameraMove;
+        }
+    }
+
+    void CameraMove(Vector2 delta) {
+        if (_cameraMovements == null) { return; }
+        if (delta.sqrMagnitude < _minDistance * _minDistance) { return; }
+        if (Mathf.Abs(delta.y) > Mathf.Abs(delta.x)) { return; }
+        if (delta.x > 0) { _cameraMovements.GoToLeft(); }
+        else { _cameraMovements.GoToRight(); }
+    }
+}
