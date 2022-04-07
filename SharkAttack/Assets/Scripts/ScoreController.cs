74f2326e1a1c7500afd27272c96bb9ac2b1d9fd6
Wizerdy <grancru2000@gmail.com>
2022-04-07 13:37:12 +0200

    Multiplicateur

--- SharkAttack/Assets/Scripts/ScoreController.cs
+++ SharkAttack/Assets/Scripts/ScoreController.cs
@@ -5,6 +5,8 @@ using UnityEngine.Events;
 
 public class ScoreController : MonoBehaviour {
     [SerializeField] int _score;
+    [SerializeField] float _multiplicateur = 1f;
+    [SerializeField] float _multiplicateurGain = 0.05f;
     [SerializeField] UnityEvent<int> _onScoreEarn;
 
     public event UnityAction<int> OnScoreEarn { add => _onScoreEarn.AddListener(value); remove => _onScoreEarn.RemoveListener(value); }
@@ -19,7 +21,13 @@ public class ScoreController : MonoBehaviour {
 
     }
 
+    public void ResetMult() {
+        _multiplicateur = 1f;
+    }
+
     public void AddScore(int score) {
+        score = Mathf.CeilToInt(score * _multiplicateur);
+        _multiplicateur += _multiplicateurGain;
         _score += score;
         _onScoreEarn?.Invoke(score);
     }
